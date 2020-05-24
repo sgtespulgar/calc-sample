cf67b201f1e1bc90fc6b2446be0e7e5e2e6ca89e
Suzerain <sgtespulgar@gmail.com>
2020-05-24 23:45:56 +0800

    Updates

---
+++ precache-manifest.07950f47cc9b918327d4248e7aa89d8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a651dac57c8da3310e17dc85ba08ab6",
+    "url": "/calc-sample/index.html"
+  },
+  {
+    "revision": "bdc4e4ddcea212792f29",
+    "url": "/calc-sample/static/css/main.8f13127a.chunk.css"
+  },
+  {
+    "revision": "fee61a179e0bd5e26b1e",
+    "url": "/calc-sample/static/js/2.bbd9de2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calc-sample/static/js/2.bbd9de2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdc4e4ddcea212792f29",
+    "url": "/calc-sample/static/js/main.bf5e3b9d.chunk.js"
+  },
+  {
+    "revision": "5070e424493b56f04aa6",
+    "url": "/calc-sample/static/js/runtime-main.7501a661.js"
+  }
+]);
